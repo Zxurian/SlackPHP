64d5cd28c18209df0fb41d7250ab5d99f77b1d58
Rick Goodrow <rgoodrow@tantor.com>
2017-05-10 16:31:56 -0400

    updating unit tests

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(function($className) {
-    $className = str_replace('\\', '/', $className);
-    $classFile = __DIR__.'/../src/'.str_replace('SlackPHP/', '', $className).'.php';
-    if (file_exists($classFile)) {
-        require $classFile;
-    }
-});
