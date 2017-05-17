99b886dafdb39c76063173ae9f084fb103ff5876
Dzianis Zhaunerchyk <dzhaunerchyk@tantor.com>
2017-05-17 10:58:00 -0400

    Tests for ChatPostMessageResponse payload model

--- src/SlackAPI/Models/Payloads/ChatPostMessageResponse.php
+++ src/SlackAPI/Models/Payloads/ChatPostMessageResponse.php
@@ -6,7 +6,7 @@ use SlackPHP\SlackAPI\Models\AbstractModels\AbstractPayloadResponse;
 use JMS\Serializer\Annotation\Type;
 
 /**
- * Class, used for deserialization of payload received after posting message to channel
+ * Class, used for deserialization of respponse payload received after posting message to channel
  * 
  * @author Dzianis Zhaunerchyk <dzhaunerchyk@gmail.com>
  */
@@ -16,47 +16,17 @@ class ChatPostMessageResponse extends AbstractPayloadResponse
      * @var string|NULL
      * @Type("string")
      */
-    private $ts = null;
+    protected $ts = null;
 
     /**
      * @var string|NULL
      * @Type("string")
      */
-    private $channel = null;
+    protected $channel = null;
 
     /**
      * @var array|NULL
      * @Type("array")
      */
-    private $message = null;
-    
-    /**
-     * Getter for ts
-     * 
-     * @return string|NULL
-     */
-    public function getTs()
-    {
-        return $this->ts;
-    }
-
-    /**
-     * Getter for channel
-     * 
-     * @return string|NULL
-     */
-    public function getChannel()
-    {
-        return $this->channel;
-    }
-    
-    /**
-     * Getter for message array
-     * 
-     * @return array
-     */
-    public function getMessage()
-    {
-        return $this->message;
-    }
+    protected $message = null;
 }
