d8d6d382431d29c0d460aa5655e384d3938e3f93
Dzianis Zhaunerchyk <dzhaunerchyk@tantor.com>
2017-05-17 14:42:30 -0400

    Tests for UsersList payload model

--- src/SlackAPI/Models/Payloads/UsersList.php
+++ src/SlackAPI/Models/Payloads/UsersList.php
@@ -3,6 +3,7 @@
 namespace SlackPHP\SlackAPI\Models\Payloads;
 
 use SlackPHP\SlackAPI\Models\AbstractModels\AbstractPayload;
+use SlackPHP\SlackAPI\Exceptions\SlackException;
 
 /**
  * This method returns a list of all users in the team.
@@ -29,10 +30,15 @@ class UsersList extends AbstractPayload
      * Whether to include presence data in the output
      *
      * @param bool $presence
+     * @throws SlackException
      * @return UsersList
      */
     public function setPresence($presence)
     {
+        if (!is_bool($presence)) {
+            throw new SlackException('Presense should be a boolean type', SlackException::NOT_BOOLEAN);
+        }
+        
         $this->presence = $presence;
     
         return $this;
