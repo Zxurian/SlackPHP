80ee13c7ee6487b93175738d08c6752c1d4ccaea
Dzianis Zhaunerchyk <evrenior@mail.ru>
2017-05-22 22:30:54 -0400

    Test for set parsedReceivedPayload event

---
+++ Tests/SlackAPI/Events/ParsedReceivedEventTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\SlackAPI\Events;
+
+use PHPUnit\Framework\TestCase;
+use SlackPHP\SlackAPI\Events\ParsedReceivedEvent;
+use SlackPHP\SlackAPI\Models\Methods\GroupsListResponse;
+
+/**
+ * @author Dzianis Zhaunerchyk <dzhaunerchyk@gmail.com>
+ * @covers ParsedReceivedEvent
+ */
+class ParsedReceivedEventTest extends TestCase
+{
+    /** 
+     * Test setting payloadResponse
+     */
+    public function testSettingPayloadResponse()
+    {
+        $payloadResponse = new GroupsListResponse();
+        $parsedReceivedEventObject = new ParsedReceivedEvent();
+        $returnedObject = $parsedReceivedEventObject->setPayloadResponse($payloadResponse);
+        $refParsedReceivedEventObject = new \ReflectionObject($parsedReceivedEventObject);
+        $payloadResponseProperty = $refParsedReceivedEventObject->getProperty('payloadResponse');
+        $payloadResponseProperty->setAccessible(true);
+        $this->assertInstanceOf(GroupsListResponse::class, $payloadResponseProperty->getValue($parsedReceivedEventObject));
+        $this->assertInstanceOf(ParsedReceivedEvent::class, $returnedObject);
+    }
+}
