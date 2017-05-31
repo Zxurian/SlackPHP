93b9fe7b264977814f015b6e1e13db805d5127a9
Rick Goodrow <rgoodrow@tantor.com>
2017-05-31 12:17:04 -0400

    typo

--- src/DeepLink/Abstracts/DeepLink.php
+++ src/DeepLink/Abstracts/DeepLink.php
@@ -13,7 +13,7 @@ use SlackPHP\DeepLink\Interfaces\LinkInterface;
  * @version 0.3
  * @see https://api.slack.com/docs/deep-linking
  */
-class DeepLink implements DeepLinkInterface
+class DeepLink implements LinkInterface
 {
     const SLACK_DEEP_LINK = 'slack://';
     
