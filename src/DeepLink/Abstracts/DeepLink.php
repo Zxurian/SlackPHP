8d87f08daf968a2b85b1f2224189eb3dcc8deeb1
Rick Goodrow <rgoodrow@tantor.com>
2017-05-31 12:25:42 -0400

    fixing abstract delcaration

--- src/DeepLink/Abstracts/DeepLink.php
+++ src/DeepLink/Abstracts/DeepLink.php
@@ -13,7 +13,7 @@ use SlackPHP\DeepLink\Interfaces\LinkInterface;
  * @version 0.3
  * @see https://api.slack.com/docs/deep-linking
  */
-class DeepLink implements LinkInterface
+abstract class DeepLink implements LinkInterface
 {
     const SLACK_DEEP_LINK = 'slack://';
     
